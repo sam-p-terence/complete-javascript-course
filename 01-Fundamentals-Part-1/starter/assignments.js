d3fb1a39c8117fbde1296877bd1b41d7d56cf358
Terence <sam.p.terence@gmail.com>
2023-03-26 15:45:25 -0700

    up to part 14

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,55 @@
+
+"LECTURE: Values and Variables"
+
+
+// let country = 'United States of California';
+// let continent = 'North America';
+// let population = '325 Million'
+
+// console.log(country, continent, population);
+
+"LECTURE: Data Types"
+
+// let isIsland = false;
+// let language
+
+// console.log(typeof isIsland, population, country, continent)
+
+// console.log(isIsland);
+// console.log(country, continent, population);
+
+
+"LECTURE: let, const and var"
+
+// language = "English";
+// const country = 'Japan';
+// const continent = 'Asia';
+// let isIsland = false;
+// isIsland = true;
+
+// console.log(country, continent, isIsland)
+
+
+"LECTURE: Basic Operators"
+
+// // const countryHalf = 1 - 1/2
+// // console.log(countryHalf)
+
+// const myPopulation = 100
+// console.log(myPopulation / 2);
+
+// // myPopulation++
+
+// const finlandPopulation = 6000000;
+// console.log(myPopulation > finlandPopulation)
+
+// const averageCountry = 33000000;
+// console.log(myPopulation < averageCountry);
+
+
+// const myCountry = "Portugal"
+// const myContinent = "Europe"
+// const population1 = "11 Million"
+
+// const description = myCountry + " Is in " + myContinent + ", and its " + population1 + " people speak portugese"
+// console.log(description)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,121 @@
+
+/////////////////////// 10. Values and Variables 
+
+
+// let js = 'amazing';
+// console.log(40+8+23-10);
+
+// console.log('Jonas');
+// console.log(23)
+
+// let firstName = 'Matilda';
+// let first = 'Jonas'
+// let firstNamePerson = 'Yoo'
+// let first_name_person = 'Yee'
+
+// console.log(firstName);
+// console.log(firstNamePerson);
+// console.log(first_name_person);
+
+
+// Variable name conventions >>>> 
+
+// let jonas_matilda = 'JM';
+// let $function = 27;
+
+// let person = 'Jonassss'
+// let PI = 3.1415;
+
+// let myFirstJob = 'coder';
+// let myCurrentJob = 'Teacher';
+
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob)
+
+
+
+///////////////////////// 12. Data Types
+
+
+// let dataTypes = [
+//     ('Number'),
+//     ('String'),
+//     ('Boolean'),
+//     ('Undefined'),
+//     ('Null'),
+//     ('Symbol (ES2015)'),
+//     ('BigInt (ES2020)')
+// ]
+
+// console.log(dataTypes)
+
+// let javascriptIsFun = true
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'Yes';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+
+//////////////////////////////// 13. let, const and var
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+// // const job;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// let lastName = 'Schement';
+// console.log(lastName)
+
+/////////////////////////////// 14. LECTURE: Basic Operators
+
+// // Math Operators
+// const now = 2037
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// // 2 ** 3 ,eams 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + " " + lastName);
+
+
+// // Assignment Operators
+// let x = 10 + 5;
+// x += 10; // x = x + 10 
+// x *= 4; // x = x * 4 = 100
+// x++;  // x = x + 1
+// x--; // x = x - 1
+// x--;
+// console.log(x);
+
+// // Comparison Operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18)
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2019)
