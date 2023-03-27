a95b26baf20ed7cd276a12ee2008943e8d13f2cb
Terence <sam.p.terence@gmail.com>
2023-03-27 16:44:19 -0700

    Arrows

---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,38 @@
+// 'use strict';
+
+"LECTURE: Functions"
+
+// function describeCountry(country, population, capitalCity) {
+//     const countryString = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//     return countryString;
+// }
+
+
+// const theCountry = describeCountry("Finland", 6, "Helsinki")
+// console.log(theCountry)
+
+// console.log(describeCountry("Finland", 6, "Helsinki"));
+// console.log(describeCountry("Netherlands", 3, "Amsterdam"));
+// console.log(describeCountry("Spain", 10, "Madrid"));
+
+"LECTURE: Function Declarations vs. Expressions"
+
+// Function Declaration
+// function percentageOfWorld1(population) {
+//     return (population/7900) * 100 ;
+// }
+
+// const populationOfChina = percentageOfWorld1(1441);
+// const populationOfJapan = percentageOfWorld1(1000);
+// const populationOfTaiwan = percentageOfWorld1(300);
+// console.log(populationOfChina, populationOfJapan, populationOfTaiwan)
+
+
+// // Function Expression
+// const percentageOfWorld2 = function(population2) {
+//     return (population2/7900) * 100;
+// }
+
+// const populationOfKorea = percentageOfWorld2(500)
+// console.log(populationOfKorea)
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,67 @@
-hello
+///////////////////////////////////////////// 32. Activating Strict Mode
+
+'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if(passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log('I can drive');
+
+//// Reserved Object names >>>>
+
+// const interface = 'Audio'
+// const private = 534
+// const if = 'yes'
+
+//////////////////////////// 33. Functions
+
+
+// Function and function data
+// function logger() {
+//     console.log('My name is Jonas')
+// };
+
+
+// // Calling / Running / Invoking the function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+    
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// // console.log(fruitProcessor(5, 0))
+
+// const appleOrangeJuice = fruitProcessor(2, 4)
+// console.log(appleOrangeJuice);
+
+// const num = Number('23');
+
+
+
+//////////////////////////////////// 34. Function Declarations vs. Expressions
+
+// // Function Declaration
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991) 
+// console.log(age1)
+
+
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1990);
+// console.log(age1, age2)
+
+
+
+//////////////////////////////////////////////////////// 35. Arrow Functions
