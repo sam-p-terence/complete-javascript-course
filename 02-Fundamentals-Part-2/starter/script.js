42a9eb7d64f627c2ed34abd36dcd6811a6aadc14
Terence <sam.p.terence@gmail.com>
2023-03-27 18:16:03 -0700

    Code challenge 1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -104,24 +104,70 @@
 ////////////////////////////////////////// 37. Reviewing Functions
 
 
-const calcAge = function(birthYear) {
-    return 2037 - birthYear
-}
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear
+// }
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-    const age = calcAge(birthYear)
-    const retirement = 65 - age;
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear)
+//     const retirement = 65 - age;
 
-    if(retirement > 0) {
-        console.log (`${firstName} retires in ${retirement} years`);
-        return retirement;
-    } else {
-        console.log(`${firstName} has already retired`);
-        return -1;
-    }
+//     if(retirement > 0) {
+//         console.log (`${firstName} retires in ${retirement} years`);
+//         return retirement;
+//     } else {
+//         console.log(`${firstName} has already retired`);
+//         return -1;
+//     }
 
-}
+// }
 
 
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+/////////////////////////////////////////// 38. Coding Challenge #1
+
+
+// function checkWinner(avgDolphins, avgKoalas) {
+//     const largest = Math.max(calculateScore)
+//     return `${largest} scored higher than the opposer!`
+// }
+
+
+// function calcAverage(name, score1, score2, score3) {
+//     const calculateScore = (score1 + score2 + score3) / 3;
+//     // console.log(calcAverage)
+//     return `${name} scored an average of ${calculateScore}`
+// }
+
+// console.log(calcAverage("Koalas", 44, 23, 71))
+// console.log(calcAverage("Dolphins", 65, 34, 49))
+
+
+
+
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// // console.log(calcAverage(3, 4 , 5))
+
+// // Test 1
+// const scoreDolphins = calcAverage(44, 23, 71)
+// const scoreKoalas = calcAverage(65, 54, 49)
+// console.log(scoreDolphins, scoreKoalas)
+
+// const checkWinner = function(avgDolphins, avgKoalas) {
+//     if (avgDolphins >= 2 * avgKoalas) {
+//         console.log(`Dolphins are winners! (${avgDolphins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas >= 2 * avgDolphins){
+//         console.log(`Koalas are winners! (${avgDolphins} vs. ${avgKoalas})`);
+//     } else {
+//         console.log('No one won')
+//     }
+// }
+
+// checkWinner(scoreDolphins, scoreKoalas);
+// checkWinner(574, 23)
+
+
+///////////////////////////////////////// 39. Introduction to Arrays
