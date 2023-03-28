b0a89b6bb0357c0441bf74110f2963275fa463d9
Terence <sam.p.terence@gmail.com>
2023-03-27 17:17:13 -0700

    finish functions within function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -31,7 +31,6 @@
 // function fruitProcessor(apples, oranges) {
 //     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
 //     return juice;
-    
 // }
 
 // const appleJuice = fruitProcessor(5, 0);
@@ -87,3 +86,17 @@
 
 
 /////////////////////////////////////////// 36. Functions Calling Other Functions
+
+
+function cutFruitPieces(fruit) {
+    return fruit * 3;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges)
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
