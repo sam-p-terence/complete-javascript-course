585e615b1b50f8f231ee4ce6996544a5e542c27d
Terence <sam.p.terence@gmail.com>
2023-03-29 15:30:51 -0700

    some styling when you win

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -72,3 +72,49 @@
 // });
 
 ////////////////////////////////////////// 75. Manipulating CSS Styles
+
+// const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// let score = 20;
+// document.querySelector('.number').textContent = secretNumber
+
+// document.querySelector('.check').addEventListener('click', function() {
+//     const guess = Number(document.querySelector('.guess').value)
+//     console.log(guess, typeof guess);
+
+
+//     // When no number is entered
+//     if(!guess) {
+//         document.querySelector('.message').textContent = '❌ No Number detected!'
+
+
+//         // When player wins
+//     } else if (guess === secretNumber) {
+//         document.querySelector('.message').textContent = '✅ Correct Number!';
+//         document.querySelector('body').style.backgroundColor = '#60b347';
+//         document.querySelector('.number').style.width = '30rem';
+
+//         // When the number is too high
+//     } else if (guess > secretNumber) {
+//         if(score > 1) {
+//             document.querySelector('.message').textContent = '❌ Too High!';
+//             score--;
+//             document.querySelector('.score').textContent = score;
+//         } else {
+//             document.querySelector('.message').textContent = 0;
+//             document.querySelector('.message').textContent = "😣 You lost the game!";
+//         }
+
+//         //When the number is too low
+//     } else if (guess < secretNumber) {
+//         if(score > 1) {
+//             document.querySelector('.message').textContent = '❌ Too Low!';
+//             score--;
+//             document.querySelector('.score').textContent = score;
+//         } else {
+//             document.querySelector('.message').textContent = 0;
+//             document.querySelector('.message').textContent = "😣 You lost the game!";
+//         }
+//     }
+// });
+
+///////////////////////////////// 76.
