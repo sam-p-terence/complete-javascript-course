e3b06a4e28e11d000f6b3c3c9e8b8ea7664980c7
Terence <sam.p.terence@gmail.com>
2023-03-29 14:26:51 -0700

    Selecting And Manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,7 +3,7 @@
 
 /////////////////////////////// 70. PROJECT #1: Guess My Number
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
 /////////////////////// 71. What's the DOM and DOM Manipulation
 
@@ -11,6 +11,15 @@ console.log(document.querySelector('.message').textContent);
 
 "API: Application Programming Interface."
 
+/////////////////////// 72. Selecting and Manipulating Elements
 
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-///////////////////////
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+//////////////////////// 73. Handling Click Events
