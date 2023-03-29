91b39914b0711ce5479060abffcb7a74bfa8a746
Terence <sam.p.terence@gmail.com>
2023-03-29 13:43:25 -0700

    71. DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+
+/////////////////////////////// 70. PROJECT #1: Guess My Number
+
+console.log(document.querySelector('.message').textContent);
+
+/////////////////////// 71. What's the DOM and DOM Manipulation
+
+"DOM is the Document Object Model: Structured Representation of HTML Document. Allows JavaScript to access HTML Elements and Styles to Manipulate them."
+
+"API: Application Programming Interface."
+
+
+
+///////////////////////
