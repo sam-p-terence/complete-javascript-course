8b4cd2134c84c85bfac41732b3a801520f1ea0da
Terence <sam.p.terence@gmail.com>
2023-03-29 14:55:17 -0700

    Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 /////////////////////////////// 70. PROJECT #1: Guess My Number
 
 // console.log(document.querySelector('.message').textContent);
@@ -13,13 +12,25 @@
 
 /////////////////////// 72. Selecting and Manipulating Elements
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
 
-//////////////////////// 73. Handling Click Events
+////////////////////////////////// 73. Handling Click Events
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess, typeof guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number detected!'
+    }
+
+});
+
+//////////////////////////////// 74. Implementing the Game Logic
