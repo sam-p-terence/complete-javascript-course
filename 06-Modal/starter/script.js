ab71d74a68148b2014e392a0cb58fec0144ce482
Terence <sam.p.terence@gmail.com>
2023-03-30 13:23:28 -0700

    Modal Logic

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,14 +2,37 @@
 
 ///////////////////////////////////// 79. Project #2. Modal Window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnsOpenModal = document.querySelectorAll('.show-modal');
+// console.log(btnsOpenModal);
 
-for(let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
-}
+// for(let i = 0; i < btnsOpenModal.length; i++) {
+//     console.log(btnsOpenModal[i].textContent);
+// }
 
 ///////////////////////////////////// 80. Working with Classes
+
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// const closeModal = function() {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// }
+
+// const openModal = function() {
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+// }
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//     btnsOpenModal[i].addEventListener('click', openModal);
+//     btnCloseModal.addEventListener('click', closeModal);
+//     overlay.addEventListener('click', closeModal);
+    
+
+///////////////////////////////// 81. Handling an "Esc" Keypress Event
