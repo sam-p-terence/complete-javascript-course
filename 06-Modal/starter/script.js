76027ca8474e761888fbddd1bd07b416fd73a8af
Terence <sam.p.terence@gmail.com>
2023-03-30 12:58:51 -0700

    Modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+///////////////////////////////////// 79. Project #2. Modal Window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for(let i = 0; i < btnsOpenModal.length; i++) {
+    console.log(btnsOpenModal[i].textContent);
+}
+
+///////////////////////////////////// 80. Working with Classes
