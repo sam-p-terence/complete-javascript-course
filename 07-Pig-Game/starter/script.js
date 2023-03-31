ab4e9aa483279656f0a5d8a4e0c88366544e59d4
Terence <sam.p.terence@gmail.com>
2023-03-30 21:39:31 -0700

    Roll Dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,9 +3,75 @@
 ////////////////////////////////// Project #3: Pig Game
 
 // Selecting Elements
+// const score0El = document.querySelector('#score--0');
+// const score1El = document.getElementById('score--1');
+// const diceEl = document.querySelector('.dice');
+
+// const btnNew= document.querySelector('.btn--new')
+// const btnRoll= document.querySelector('.btn--roll')
+// const btnHold= document.querySelector('.btn--hold')
+
+
+// // Starting Conditions
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add('hidden');
+
+/////////////////////////////////////// 83. Rolling the Dice
+
+// Selecting Elements
+// const score0El = document.querySelector('#score--0');
+// const score1El = document.getElementById('score--1');
+// const current0El = document.getElementById('current--0');
+// const current1El = document.getElementById('current--1');
+// const diceEl = document.querySelector('.dice');
+// const btnNew= document.querySelector('.btn--new');
+// const btnRoll= document.querySelector('.btn--roll');
+// const btnHold= document.querySelector('.btn--hold');
+
+
+// // Starting Conditions
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add('hidden');
+
+// let currentScore = 0;
+
+// // Dice Rolling Logic
+// btnRoll.addEventListener('click', function() {
+//     // 1. Generate Random dice roll
+//     const dice = Math.trunc(Math.random() * 6) + 1;
+//     console.log(dice)
+
+//     // 2. Display dice
+//     diceEl.classList.remove('hidden');
+//     diceEl.src = `dice-${dice}.png`;
+
+//     // 3. Check for rolled 1: if true, switch to next player
+//     if(dice !== 1) {
+//         // Add dice to current score
+//         currentScore += dice;
+//         current0El.textContent = currentScore;
+//     } else {
+//         // Switch to next player
+
+
+//     }
+// })
+
+
+
+//////////////////////////// 84. Switching the Active Player
+
+// Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew= document.querySelector('.btn--new');
+const btnRoll= document.querySelector('.btn--roll');
+const btnHold= document.querySelector('.btn--hold');
 
 
 // Starting Conditions
@@ -13,4 +79,26 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-/////////////////////////////////////// 83. Rolling the Dice
+let currentScore = 0;
+
+// Dice Rolling Logic
+btnRoll.addEventListener('click', function() {
+    // 1. Generate Random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice)
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+    if(dice !== 1) {
+        // Add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore;
+    } else {
+        // Switch to next player
+        
+
+    }
+})
