56fbf3eef0799d177fba87635bb47b954583d0e8
Terence <sam.p.terence@gmail.com>
2023-04-01 23:52:19 -0700

    Primitives

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -72,5 +72,110 @@
 // const z = 3;
 
 // console.log(x === window.x);
-// console.log(y === window.y);
+// console.log(y === window.y); 
 // console.log(z === window.z);
+
+//////////////////////////////////////// 97. The  / this / keyword in practice
+
+// console.log(this);
+
+// const calcAge = function(birthYear) {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+// calcAgeArrow(1980);
+
+// const jonas = {
+//     year: 1991,
+//     calcAge: function() {
+//         console.log(this);
+//         console.log(2037 - this.year)
+//     }
+// }
+// jonas.calcAge();
+
+
+// const matilda = {
+//     year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f(); 
+
+
+/////////////////////////////////////// 98. Regular Functions vs. Arrow Functions
+
+// var firstName = 'Matilda';
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1991,
+//     calcAge: function() {
+//         // console.log(this);
+//         console.log(2037 - this.year);
+
+//         // Solution 1
+//         // const self = this;
+//         // const isMillenial = function() {
+//         //     console.log(self)
+//         //     console.log(self.year >= 1981 ** self.year <= 1996)
+//         // };
+
+//         // Solution 2
+//         const self = this;
+//         const isMillenial = () => {
+//             console.log(self)
+//             console.log(self.year >= 1981 ** self.year <= 1996)
+//         };
+//         isMillenial()
+//     },
+
+//     greet: () => { 
+//         console.log(this)
+//         console.log(`Hey ${this.firstName}`)
+//     },
+// };
+// jonas.greet();
+// jonas.calcAge();
+
+
+// const addExpr = function (a, b) {
+//     console.log(arguments)
+//         return a + b;
+//     };
+// addExpr(2, 5)
+    
+// var addArrow = (a, b) => {
+//     console.log(arguments)
+//     return a + b;
+// };
+// addArrow(2, 5, 8)
+
+////////////////////////// 99. Primitives vs. Objects (Primitive vs. Referense Types)
+
+// let age = 30;
+// let oldAge = 30;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+// const me = {
+//     name: "Jonas",
+//     age: 30,
+// };
+
+// const friend = me;
+// friend.age = 27;
+// console.log('Friends', friend);
+// console.log('Me', me)
+
+//////////////////////////////////// 100. Primitives vs. Objects in Practice
