597a2b6ab0defcc575ea07a99a3c3659c115d941
Terence <sam.p.terence@gmail.com>
2023-04-01 22:49:52 -0700

    update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,29 +1,43 @@
 'use strict';
 
-function calcAge(birthYear) {
-    const age = 2037 - birthYear;
 
-    function printAge() {
-        let output = `${firstName}, is ${age}, and was born in ${birthYear}`
-        console.log(output);
+//////////////////////////////////////// 93. Scoping in Practice
 
-        if(birthYear >= 1981 && birthYear <= 1996) {
-            const firstName = "Steven"
-            const str = `Oh, and you're a millenial, ${firstName}`
-            console.log(str);
+// function calcAge(birthYear) {
+//     const age = 2037 - birthYear;
 
-            function add(a, b) {
-                return a + b;
-            };
+//     function printAge() {
+//         let output = `${firstName}, is ${age}, and was born in ${birthYear}`
+//         console.log(output);
+
+//         if(birthYear >= 1981 && birthYear <= 1996) {
+//             const firstName = "Steven"
+//             const str = `Oh, and you're a millenial, ${firstName}`
+//             console.log(str);
+
+//             function add(a, b) {
+//                 return a + b;
+//             };
             
-            output = 'New Output'
-        };
-        console.log(output)
-    }
-    printAge();
-    return age;
-}
+//             output = 'New Output'
+//         };
+//         console.log(output)
+//     }
+//     printAge();
+//     return age;
+// }
 
-const firstName = "Jonas";
-calcAge(1991);
+// const firstName = "Jonas";
+// calcAge(1991);
 
+//////////////////////////////////// 95. Hoisting and TDZ in Practice
+
+console.log(me)
+console.log(job)
+console.log(birthYear)
+
+
+
+var me = "Jonas";
+let job = "teacher";
+const birthYear = 1991;
