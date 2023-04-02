6407cdec52707757cc16c128476638ad3381f911
Terence <sam.p.terence@gmail.com>
2023-04-02 00:04:43 -0700

    Primitives vs. Objects in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -179,3 +179,40 @@
 // console.log('Me', me)
 
 //////////////////////////////////// 100. Primitives vs. Objects in Practice
+
+
+// // Primitive Types
+// let lastName = "Williams";
+// let oldLastName = lastName;
+// lastName = 'Davis';
+// console.log(lastName, oldLastName);
+
+// // Reference Types
+// const jessica = {
+//     firstName: "Jessica",
+//     lastName: "Williams",
+//     age: 27,
+// };
+// const marriedJessica = jessica;
+// marriedJessica.lastName = "Davis";
+// console.log("Before marriage:", jessica);
+// console.log("After marriage:", marriedJessica);
+
+// // marriedJessica = {};
+
+// // Copying Objects
+// const jessica2 = {
+//     firstName: "Jessica",
+//     lastName: "Williams",
+//     age: 27,
+//     family: ['Alice', 'Bob']
+// };
+
+// const jessicaCopy = Object.assign({}, jessica2);
+// jessicaCopy.lastName = "Davis";
+
+// jessicaCopy.family.push('Mary');
+// jessicaCopy.family.push('John')
+
+// console.log("Before marriage:", jessica2);
+// console.log("After marriage:", jessicaCopy);
