9858ec78c6444f6e2bbf75068582ec2852d23ba9
Terence <sam.p.terence@gmail.com>
2023-04-01 22:28:18 -0700

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        let output = `${firstName}, is ${age}, and was born in ${birthYear}`
+        console.log(output);
+
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            const firstName = "Steven"
+            const str = `Oh, and you're a millenial, ${firstName}`
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            };
+            
+            output = 'New Output'
+        };
+        console.log(output)
+    }
+    printAge();
+    return age;
+}
+
+const firstName = "Jonas";
+calcAge(1991);
+
