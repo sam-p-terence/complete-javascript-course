0c3442d831d3a28ca1d71665f429e0859047abdc
Terence <sam.p.terence@gmail.com>
2023-04-03 18:59:55 -0700

    destructure arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -14,39 +14,63 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  }
+
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
 };
 
 
 // const arr = [2,3,4];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
+
+// const a = [0];
+// const b = [1];
+// const c = [2];
 
 // const [x, y, z] = arr;
 // console.log(x, y, z);
 // console.log(arr);
 
-let [main, ,secondary] = restaurant.categories;
-console.log(main, secondary);
-
-// const temp = main;
-// main = secondary;
-// secondary = temp;
+// let [main, , secondary] = restaurant.categories;
 // console.log(main, secondary);
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary)
+// // Switching variables
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// // console.log(restaurant.order(2, 0));
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested;
+// // console.log( i, j)
+
+// const [i, ,[ j, k]] = nested;
+// console.log( i, j, k);
+
+// //default values
+// const [p = 1, q= 1, r= 1] = [8, 9];
+// console.log( p, q, r);
+
+////////////////////////// 104. Destructuring Objects
