a6c387277f66568a72ef35b46d4941eba418eca4
Terence <sam.p.terence@gmail.com>
2023-04-07 21:23:57 -0700

    Optional Chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -638,3 +638,56 @@ const flights =
 
 //////////////////////////////////////// 112. Enhanced Object Literals
 
+// const weekdays = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+//   [`day-${2 + 4}`]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
+
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+//     //ES6 Enhanced Object Literals
+//   openingHours,
+    
+//     order(starterIndex, mainIndex) {
+//       return [this.starterMenu[starterIndex], 
+//       this.mainMenu[mainIndex]];
+//     },
+    
+//     orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+//       console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to "${address}" at ${time}`);
+//     },
+    
+//     orderPasta(ing1, ing2, ing3) {
+//       console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+//     },
+
+//     orderPizza(mainIngredient, ...otherIngredient) {
+//       console.log(mainIngredient);
+//       console.log(otherIngredient);
+//     },
+// };
+
+
+////////////////////////////////////// 113. Optional Chaining
+
