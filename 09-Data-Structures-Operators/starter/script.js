472793bde1010c889d8d4ba024a926c90d389c66
Terence <sam.p.terence@gmail.com>
2023-04-07 21:39:52 -0700

    Looping Objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -689,5 +689,80 @@ const flights =
 // };
 
 
-////////////////////////////////////// 113. Optional Chaining
+////////////////////////////////////// 113. Optional Chaining (?.)
 
+
+// const weekdays = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
+
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+//     //ES6 Enhanced Object Literals
+//   openingHours,
+    
+//     order(starterIndex, mainIndex) {
+//       return [this.starterMenu[starterIndex], 
+//       this.mainMenu[mainIndex]];
+//     },
+    
+//     orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+//       console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to "${address}" at ${time}`);
+//     },
+    
+//     orderPasta(ing1, ing2, ing3) {
+//       console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+//     },
+
+//     orderPizza(mainIngredient, ...otherIngredient) {
+//       console.log(mainIngredient);
+//       console.log(otherIngredient);
+//     },
+// };
+
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+// console.log(restaurant.openingHours.mon.open);
+
+// // WITH Optional Chaining (?.)
+
+// console.log(restaurant.openingHours.mon?.open)
+// console.log(restaurant.openingHours?.mon?.open)
+
+// // EXAMPLE
+// const days = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   console.log(day);
+//   const open = restaurant.openingHours[day]?.open ?? 'closed'
+//   console.log(`on ${day}, we open at ${open}`);
+// };
+
+// // Methods
+// console.log(restaurant.order?.(0,1) ?? 'Method does not exist');
+
+// // Arrays
+// const users = [
+//   {name: 'Jonas', email: 'hello@jonas.io'}
+// ];
+
+// console.log(users[0]?.name ?? 'User array is empty')
+
+///////////////////////////////// 114. Looping Objhects: Object Keys, Values, Entries
