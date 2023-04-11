6dc0304c8427b8985c1e3bb6d815dd4fcfa79eb2
Terence <sam.p.terence@gmail.com>
2023-04-11 14:32:32 -0700

    .

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1109,3 +1109,31 @@ const restaurant = {
       console.log(otherIngredient);
     },
 };
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['Correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try Again!'],
+]);
+console.log(question);
+
+// Convert Object to MAP
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+// Quiz App
+console.log(question.get('question'))
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer)
+
+
+console.log(question.get(question.get('correct') === answer))
