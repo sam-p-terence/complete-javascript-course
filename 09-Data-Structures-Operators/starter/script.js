2ef63686b485e3942f046d07801a34e3ed40dd64
Terence <sam.p.terence@gmail.com>
2023-04-11 14:17:31 -0700

    Maps Iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -838,7 +838,7 @@ const flights =
 //   console.log(`On ${key} we open at ${open} and close at ${close}`)
 // };
 
-/////////////////////// Coding Challenge 2
+/////////////////////////////////////////////////// Coding Challenge 2
 
 
 // const players = [
@@ -984,8 +984,85 @@ const flights =
 // console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size)
 // console.log(new Set('jonasschmedtmann').size);
 
-///////////////////////////////// 117. Maps: Fundamentals
 
+//////////////////////////////////////////////// 117. Maps: Fundamentals
+
+
+// const weekdays = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [weekdays[5]]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
+
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+//     //ES6 Enhanced Object Literals
+//   openingHours,
+    
+//     order(starterIndex, mainIndex) {
+//       return [this.starterMenu[starterIndex], 
+//       this.mainMenu[mainIndex]];
+//     },
+    
+//     orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+//       console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to "${address}" at ${time}`);
+//     },
+    
+//     orderPasta(ing1, ing2, ing3) {
+//       console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+//     },
+
+//     orderPizza(mainIngredient, ...otherIngredient) {
+//       console.log(mainIngredient);
+//       console.log(otherIngredient);
+//     },
+// };
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Fireenze, Italy');
+// rest.set(2, 'Lisbon, Portugal');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed')
+
+// console.log(rest.get('name'))
+// console.log(rest.get(true))
+// console.log(rest.get(1))
+
+// const time = 8;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest)
+// console.log(rest.size)
+
+// console.log(rest.get([1,2]))
+
+
+//////////////////////////////////// 118. Maps: Iteration
 
 const weekdays = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
@@ -1032,33 +1109,3 @@ const restaurant = {
       console.log(otherIngredient);
     },
 };
-
-
-const ordersSet = new Set ([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza'
-]);
-console.log(ordersSet);
-
-console.log(new Set('Jonas'));
-
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-ordersSet.delete('Risotto');
-console.log(ordersSet);
-
-for (const order of ordersSet) console.log(order);
-
-// Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size)
-console.log(new Set('jonasschmedtmann').size);
