a01aeeb22c20d2ddd2974a1739090186caa23505
Terence <sam.p.terence@gmail.com>
2023-04-11 13:52:19 -0700

    .

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -837,3 +837,72 @@ const flights =
 // for( const [key, {open, close}] of entries) {
 //   console.log(`On ${key} we open at ${open} and close at ${close}`)
 // };
+
+/////////////////////// Coding Challenge 2
+
+
+// const players = [
+//   "Davies",
+//   "Muller",
+//   "Lewandowski",
+//   "Kimmich",
+// ];
+
+// let team1 = [];
+// let team2 = [];
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+    'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnarby',
+    'Lewandowski',
+    ],
+    [
+    'Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+    ],
+  ],
+    score: '4:0',
+    scored: 
+      ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+  };
+
+// 1.
+for(const [i, player] of game.scored.entries()) 
+console.log(`Goal ${i + 1}: ${player}`)
+
+// 2.
+let average = 0;
+for (const odd of Object.values(game.odd))
+  average += odd;
+  average /= 
+  console.log(average)
+
+  
