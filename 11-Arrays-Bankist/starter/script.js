658e60fa7d0509c710e31ee1e8cf2e0160b92af2
Terence <sam.p.terence@gmail.com>
2023-04-12 14:15:53 -0700

    at method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -111,3 +111,17 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 
 ///////////////////////////////// 143. The new at Method
+
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// // Getting the last array element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// console.log('jonas'.at(0));
+// console.log('jonas'.at-10));
+
+///////////////////////////////// 144. Looping Arrays: forEach
