50225e76db4ea8fbd2ae73b0d914815366e6ab4c
Terence <sam.p.terence@gmail.com>
2023-04-12 14:10:31 -0700

    simple array method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -73,4 +73,41 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+///////////////////////////////////////////////// 142. Simple Array Methods
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+
+// // Slice
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice());
+// console.log([...arr]);
+
+// // Splice
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(1, 2);
+// console.log(arr);
+
+// // Reverse
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f']
+// console.log(arr2.reverse());
+// console.log(arr2);
+// // console.log(arr, arr2.reverse())
+
+// // Concat
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// // Join
+// console.log(letters.join(' - '))
+
+
+///////////////////////////////// 143. The new at Method
